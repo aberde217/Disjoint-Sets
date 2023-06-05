8eff0d19f89537f06049b3eb7718fb12a9ebb908
aberde217 <115136078+aberde217@users.noreply.github.com>
2023-06-05 12:53:58 -0700

    Update DSQuickUnion.java

--- DSQuickUnion.java
+++ DSQuickUnion.java
@@ -1,6 +1,7 @@
 public class DSQuickUnion implements DisjointSet {
     private int[] ds;
-
+    
+    /*Time Complexity: θ(N)*/
     public DSQuickUnion(int size) {
         ds = new int[size];
         for (int i = 0; i < ds.length; i++) {
@@ -15,14 +16,16 @@ public class DSQuickUnion implements DisjointSet {
         }
         return next;
     }
-
+    
+    /*Time Complexity: θ(N)*/
     @Override
     public void connect(int x, int y) {
         int xParent = findParent(x);
         int yParent = findParent(y);
         ds[yParent] = xParent;
     }
-
+    
+    /*Time Complexity: θ(N)*/
     @Override
     public boolean isConnected(int x, int y) {
         return findParent(x) == findParent(y);
